7a8d70b4c9c93b239033aa797a488e82852e658c
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2017-07-07 15:07:32 +0000

    Display ahead/behind counts compared to origin in zsh git prompt.

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -37,9 +37,12 @@ PROMPT="%{$fg_bold[blue]%}%%%{$reset_color%} "
 setopt PROMPT_SUBST
 autoload -Uz vcs_info
 zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:*' actionformats '%F{3}%s%f: [%b]'
-zstyle ':vcs_info:*' formats '%F{3}%s%f: [%b] %F{2}%c%F{3}%u%f'
+zstyle ':vcs_info:*' actionformats \
+    '[%F{255}%s%f:%F{11}%b%f %F{12}%c%F{3}%u%f %F{14}%m%f %F{1}%a%f]'
+zstyle ':vcs_info:*' formats \
+    '[%F{255}%s%f:%F{11}%b%f %F{12}%c%F{3}%u%f %F{14}%m%f]'
 zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
+zstyle ':vcs_info:git*+set-message:*' hooks git-remote-status
 zstyle ':vcs_info:*' enable git
 +vi-git-untracked() {
     local wt=$(git rev-parse --is-inside-work-tree 2> /dev/null)
@@ -49,6 +52,21 @@ zstyle ':vcs_info:*' enable git
     if [[ $wt == 'true' ]] && [[ $gl -ge 1 ]]; then
         hook_com[unstaged]+='%F{1}?%f'
     fi
+}
+
++vi-git-remote-status() {
+    local ahead behind
+    local -a gitstatus
+
+    ahead=$(git rev-list ${hook_com[branch]}@{upstream}..HEAD 2> /dev/null \
+        | wc -l)
+    (( $ahead )) && gitstatus+=( "+${ahead}" )
+
+    behind=$(git rev-list HEAD..${hook_com[branch]}@{upstream} 2> /dev/null \
+        | wc -l)
+    (( $behind )) && gitstatus+=( "-${behind}" )
+
+    hook_com[misc]+=${(j:/:)gitstatus}
 }
 
 precmd() { vcs_info }
