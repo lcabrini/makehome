4ba8452658f2d9a6b723251cb47440a65e1bdc61
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2017-08-11 22:26:58 +0000

    Moved aliases from zshrc to zshenv.

--- zsh/zshenv.zsh
+++ zsh/zshenv.zsh
@@ -4,6 +4,32 @@
 #
 # Maintainer: Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
 
+# We need to know which system we are on.
+if [[ $(uname) == "Linux" ]]; then
+    sys=linux
+elif [[ $(uname) == "FreeBSD" ]]; then
+    sys=bsd
+fi
+
+# Aliases
+if [[ $sys == linux ]]; then
+    alias ls='ls --color -F'
+elif [[ $sys == bsd ]]; then
+    alias ls='ls -FG'
+fi
+alias ll='ls -l'
+alias la='ls -A'
+alias h=history
+alias tree='tree -CF'
+alias gc='git commit'
+alias gst='git status'
+alias gp='git push'
+alias gpl='git pull'
+alias ga='git add'
+alias gl='git log'
+alias gd='git diff'
+alias yd='youtube-dl -f best -o "%(title)s-%(id)s.%(ext)s"'
+
 typeset -U path
 path=(~/.local/bin $path)
 
--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -4,37 +4,11 @@
 #
 # Author: Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
 
-# We need to know which system we are on.
-if [[ $(uname) == "Linux" ]]; then
-    sys=linux
-elif [[ $(uname) == "FreeBSD" ]]; then
-    sys=bsd
-fi
-
 if [[ -d ~/Git ]]; then
     cdpath=(.. ~ ~/Git)
 elif [[ -d ~/git ]]; then
     cdpath=(.. ~ ~/git)
 fi
-
-# Aliases
-if [[ $sys == linux ]]; then
-    alias ls='ls --color -F'
-elif [[ $sys == bsd ]]; then
-    alias ls='ls -FG'
-fi
-alias ll='ls -l'
-alias la='ls -A'
-alias h=history
-alias tree='tree -CF'
-alias gc='git commit'
-alias gst='git status'
-alias gp='git push'
-alias gpl='git pull'
-alias ga='git add'
-alias gl='git log'
-alias gd='git diff'
-alias yd='youtube-dl -f best -o "%(title)s-%(id)s.%(ext)s"'
 
 # History
 setopt HIST_IGNORE_ALL_DUPS
