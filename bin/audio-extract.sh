4d65cbf7640bdbdfd2a3203939fc0180c27491b7
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2020-03-07 15:06:43 +0000

    Made audio-extract POSIX-compliant (I hope).

--- bin/audio-extract.sh
+++ bin/audio-extract.sh
@@ -1,14 +1,17 @@
-#! /usr/bin/env zsh
+#! /usr/bin/env sh
 
 # Naive audio extractor.
 # Author: Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
 
-if [[ -z $(whence ffprobe) ]]; then
-    print "You must have ffmpeg installed to use this." >&2
+if [ -z "`command -v ffprobe`" ]; then
+    printf "You must have ffmpeg installed to use this." >&2
 fi
 
-for f in *; do
-    ext=$(ffprobe $f 2>&1 | grep Audio | awk '{ print $4 }')
-    out=$(print ${f:r} | tr -c 'A-Za-z0-9_-' _).$ext
-    ffmpeg -i $f -vn -acodec copy $out
+for video_file in *; do
+    #video_ext=$(printf "%s\n" "$video_file" | awk -F . '{ print $NF }')
+    video_bn=${video_file%.*}
+    audio_ext=$(ffprobe "$video_file" 2>&1 | grep Audio | awk '{ print $4 }')
+    audio_file=$(echo "$video_bn" | tr -c 'A-Za-z0-9_-' _ | tr A-Z a-z |
+                     tr -s _ | sed -e 's/^_//' -e 's/_$//').$audio_ext
+    ffmpeg -i "$video_file" -vn -acodec copy $audio_file
 done
