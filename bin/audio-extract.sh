e87380ee68fa939cdbfa138ff9e78578839f9aef
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2020-03-08 19:37:32 +0000

    Fixed multiple matches for 'grep Audio'.

--- bin/audio-extract.sh
+++ bin/audio-extract.sh
@@ -9,7 +9,7 @@ fi
 
 for video_file in *; do
     video_bn=${video_file%.*}
-    audio_ext=$(ffprobe "$video_file" 2>&1 | grep Audio | awk '{ print $4 }')
+    audio_ext=$(ffprobe "$video_file" 2>&1 | grep Audio: | awk '{ print $4 }')
     audio_file=$(echo "$video_bn" | tr -c 'A-Za-z0-9_-' _ | tr A-Z a-z |
                      tr -s _ | sed -e 's/^_//' -e 's/_$//').$audio_ext
     ffmpeg -i "$video_file" -vn -acodec copy $audio_file
