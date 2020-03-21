fcdf84357b172e2fefedda07de4b34f81a6dc1f6
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2020-03-21 20:10:46 +0000

    Updated vim-packages.

--- bin/vim-packages.sh
+++ bin/vim-packages.sh
@@ -3,27 +3,28 @@
 # Install and update vim packages.
 # Author: Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
 
-# TODO: this should be a bit more flexible, but works for now
-package_dir=$HOME/.vim/pack/default/start
+# Filetype plugins should typically go into the start directory
+package_root=$HOME/.vim/pack/default/start
 
-test ! -d $package_dir && mkdir -p $package_dir
+test ! -d $package_root && mkdir -p $package_root
 while read package; do
     package_name=$(basename $package)
-    if [[ ! -d $package_dir/$package_name ]]; then
-        cd $package_dir
+    if [[ ! -d $package_root/$package_name ]]; then
+        cd $package_root
         git clone $package.git
     else
-        cd $package_dir/$package_name
+        cd $package_root/$package_name
         git pull
     fi
 done <<EOF
 git@github.com:GrAndSE/genie-script-vim-syntax
 git@github.com:JuliaEditorSupport/julia-vim
 git@github.com:udalov/kotlin-vim
-git@github.com:itchyny/lightline.vim
 git@github.com:nelsyeung/twig.vim
 git@github.com:arrufat/vala.vim
 git@github.com:rhysd/vim-crystal
 git@github.com:elixir-lang/vim-elixir
 git@github.com:cespare/vim-toml
 EOF
+
+# TODO: add packages that should go into opt, if needed.
