68f2261c8975ddceee5babcce87e50c634f3cbee
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2020-04-03 22:16:36 +0000

    Orgified emacs initialization.

--- emacs/init.el
+++ emacs/init.el
@@ -1,71 +1,17 @@
 ;; Personal Emacs configuration
-;; Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
+;; Author: Lorenzo Cabrini <lorenzo.cabrini@gmail.com
 
-;; Package management
 (require 'package)
-(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-			 ("melpa" . "https://melpa.org/packages/")
-			 ("melpa-stable" . "https://stable.melpa.org/packages/")))
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-;; Set up UTF-8 environment
-(set-language-environment "UTF-8")
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
 
-;; Start maximized
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
-
-;; User interface
-(blink-cursor-mode 0)  ; Cursor, don't blink
-(tool-bar-mode -1)
-
-;; Theme
-(load-theme 'wheatgrass t)
-
-;; Powerline
-(require 'powerline)
-(powerline-default-theme)
-
-;; Smart tabs
-;; (smart-tabs-insinuate 'c)
-;; (setq-default indent-tabs-mode nil)
-
-;; C
-(add-hook 'c-mode-common-hook
-	  (lambda () (setq indent-tabs-mode t
-			   c-basic-offset 4
-			   tab-width 4
-			   c-default-style "bsd")))
-
-;; LISP
-(add-hook 'emacs-lisp-mode-hook
-	  (lambda() (setq indent-tabs-mode t)))
-
-;; Go
-(add-hook 'go-mode-hook
-	  (lambda () (setq indent-tabs-mode t
-			   tab-width 4)
-			   (add-hook 'before-save-hook 'gofmt-before-save)))
-
-;; web-mode
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(setq web-mode-enable-engine-detection t)
-(defun my-web-mode-hook ()
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-markup-css-indent-offset 2))
-(add-hook 'web-mode-hook 'my-web-mode-hook)
-
-;; Org mode
-(setq org-agenda-files '("~/Appunti/organizer.org"
-			 "~/Appunti/anki.org"))
-
-(setq org-log-done 'time)
-(setq org-todo-keywords
-      '((sequence "TODO(t)" "STARTED(s)" "WAITING(w)" "|" "DONE(d)")))
-
-;; Gnus
-(setq nnrss-directory "~/news/rss")
-
-(custom-set-variables
- '(safe-local-variable-values (quote ((engine . django)))))
-
+(require 'org)
+(org-babel-load-file (expand-file-name "~/.emacs.d/settings.org"))
