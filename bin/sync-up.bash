3934ea7c1e8d8ca88822f931e5b73c29c0b4fd55
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2021-02-06 16:08:31 +0000

    Minor change to sync-up script

--- bin/sync-up.bash
+++ bin/sync-up.bash
@@ -1,14 +1,14 @@
 #! /bin/bash
 
 targets="10:4a:7d:15:64:d6 ac:7b:a1:d3:b6:02"
-dirs="$HOME/org/ $HOME/.gnupg/ $HOME/.password-store/"
+dirs="$HOME/org $HOME/.gnupg $HOME/.password-store"
 
 for target in $targets; do
     line=$(ip neigh | grep $target)
     if [[ -n $line ]]; then
 	ip=$(echo $line | awk '{ print $1 }')
 	for dir in $dirs; do
-	    rsync -a $dir $ip:${dir%/}
+	    rsync -a $dir/ $ip:$dir
 	done
     fi
 done
