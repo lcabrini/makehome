74ee5811ecb2f3cb093b34fa1a49897368e76e7b
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2021-02-07 11:34:14 +0000

    Refresh ARP tables before sync in sync-up

--- bin/sync-up.bash
+++ bin/sync-up.bash
@@ -3,6 +3,9 @@
 targets="10:4a:7d:15:64:d6 ac:7b:a1:d3:b6:02"
 dirs="$HOME/org $HOME/.gnupg $HOME/.password-store"
 
+# Make sure the ARP tables are refreshed
+nmap -sn $(ip -br addr | awk '/UP/ { print $3 }') > /dev/null 2>&1
+
 for target in $targets; do
     line=$(ip neigh | grep $target)
     if [[ -n $line ]]; then
