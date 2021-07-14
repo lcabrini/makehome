d18728a391b03c1160a9e5e5d8f4ec98063fa4ba
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2021-07-14 11:39:40 +0000

    Comment SDL_VIDEODRIVER for now as it doesn't work

--- bin/startsway.bash
+++ bin/startsway.bash
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-export SDL_VIDEODRIVER=wayland
+# Using the wayland video driver for SDL doesn't work
+#export SDL_VIDEODRIVER=wayland
 export XDG_CURRENT_DESKTOP=sway
 export MOZ_ENABLE_WAYLAND=1
 eval $(ssh-agent)
